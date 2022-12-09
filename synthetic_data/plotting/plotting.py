f36022c4151cb3f370e8964ddd763c08135fa2de
aa.antonov1 <a.antonov@niuitmo.ru>
2022-12-09 16:50:13 +0300

    Fixes

--- synthetic_data/plotting/plotting.py
+++ synthetic_data/plotting/plotting.py
@@ -7,15 +7,20 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
+from scipy.stats import ttest_ind
+
 
 matplotlib.rcParams.update({'font.size': 14})
 
-NUM_OF_EXPERIMENTS = 24
+NUM_OF_EXPERIMENTS = 48
 DATA_PATH = 'results/'
-SAVE = False
+SAVE = True
 
 DATASETS = [
-    ('Income', 'gender', 'capital-gain')
+    ('Income', 'gender', 'capital-gain'),
+    ('ClimateData', None, 'meantemp'),
+    ('SnP', None, 'High'),
+    ('CreditRecord', 'FLAG_OWN_REALTY', 'AMT_INCOME_TOTAL')
 ]
 METHODS = [
     'CTGAN',
@@ -24,8 +29,8 @@ METHODS = [
     #'TabFairGanConsistent'
 ]
 
-CLS_METHODS = ['Boosting']
-REG_METHODS = ['Random Forest']
+CLS_METHODS = ['Boosting classifier']
+REG_METHODS = ['Boosting regressor']
 CLS_METRICS = ['f1', 'precision', 'recall', 'accuracy']
 REG_METRICS = ['explained_variance', 'max_error', 'r2', 'mean_squared_error']
 
@@ -65,13 +70,21 @@ def box_plot(
         real_values: pd.DataFrame,
         df: pd.DataFrame,
         methods: List[str],
+        metrics: List[str],
         efficacy_method: str,
         fig_path: str
 ):
-    for i, metric in enumerate(CLS_METRICS):
+    for i, metric in enumerate(metrics):
         metric_values = df[df['gener. method'].isin(methods)][['Exp. number', 'gener. method', metric]]
         metric_values = metric_values.groupby(['Exp. number', 'gener. method'])[metric].first().unstack()
-        metric_values.plot(kind='box', title=f'{efficacy_method}\n{metric}')
+        p_value = ttest_ind(
+            metric_values.iloc[:, 0].to_numpy().flatten(),
+            metric_values.iloc[:, 1].to_numpy().flatten()
+        ).pvalue
+        metric_values.plot(
+            kind='box',
+            title=f'{efficacy_method}\n{metric} (p_value={round(p_value, 3)})'
+        )
         real_value = real_values[metric][0]
         plt.axhline(y=real_value, label=f"Real {metric} value", color='red')
         if SAVE:
@@ -86,30 +99,53 @@ def main():
         result_path = path.join(DATA_PATH, dataset_name + '/')
         plots_path = path.join(result_path + '/plots')
         pathlib.Path(plots_path).mkdir(parents=True, exist_ok=True)
-        cls_df = pd.read_csv(path.join(result_path, dataset_name + '_classification.csv'))
-        reg_df = pd.read_csv(path.join(result_path, dataset_name + '_regression.csv'))
 
-        cls_real_result = cls_df[cls_df['gener. method'] == 'Real']
-        for cls_method in CLS_METHODS:
-            exp_df = cls_df[cls_df['class. method'] == cls_method]
-            for metric in CLS_METRICS:
-                pass
-                time_plot(
-                    real_value=cls_real_result[metric],
+        if cls_column:
+            cls_df = pd.read_csv(path.join(result_path, dataset_name + '_classification.csv'))
+            cls_real_result = cls_df[cls_df['gener. method'] == 'Real']
+            for cls_method in CLS_METHODS:
+                exp_df = cls_df[cls_df['class. method'] == cls_method]
+                for metric in CLS_METRICS:
+                    time_plot(
+                        real_value=cls_real_result[metric],
+                        df=exp_df,
+                        methods=METHODS,
+                        metric_name=metric,
+                        efficacy_method=cls_method,
+                        num_of_experiments=NUM_OF_EXPERIMENTS,
+                        fig_path=plots_path
+                    )
+                box_plot(
+                    real_values=cls_real_result,
                     df=exp_df,
                     methods=METHODS,
-                    metric_name=metric,
+                    metrics=CLS_METRICS,
                     efficacy_method=cls_method,
-                    num_of_experiments=NUM_OF_EXPERIMENTS,
                     fig_path=plots_path
                 )
-            box_plot(
-                real_values=cls_real_result,
-                df=exp_df,
-                methods=METHODS,
-                efficacy_method=cls_method,
-                fig_path=plots_path
-            )
+        if reg_column:
+            reg_df = pd.read_csv(path.join(result_path, dataset_name + '_regression.csv'))
+            reg_real_result = reg_df[reg_df['gener. method'] == 'Real']
+            for reg_method in REG_METHODS:
+                exp_df = reg_df[reg_df['regg. method'] == reg_method]
+                for metric in REG_METRICS:
+                    time_plot(
+                        real_value=reg_real_result[metric],
+                        df=exp_df,
+                        methods=METHODS,
+                        metric_name=metric,
+                        efficacy_method=reg_method,
+                        num_of_experiments=NUM_OF_EXPERIMENTS,
+                        fig_path=plots_path
+                    )
+                box_plot(
+                    real_values=reg_real_result,
+                    df=exp_df,
+                    methods=METHODS,
+                    metrics=REG_METRICS,
+                    efficacy_method=reg_method,
+                    fig_path=plots_path
+                )
 
 
 if __name__ == "__main__":
--- synthetic_data/profiling/profiling.py
+++ synthetic_data/profiling/profiling.py
@@ -8,30 +8,38 @@ from random import randint
 
 DATA_PATH = 'data/_generated/'
 RESULTS_PATH = 'results/'
-DATASETS = ['Income']
+DATASETS = [
+    ('Income', 'gender', 'capital-gain'),
+    ('ClimateData', None, 'meantemp'),
+    ('SnP', None, 'High'),
+    ('CreditRecord', 'FLAG_OWN_REALTY', 'AMT_INCOME_TOTAL')
+]
 METHODS = [
     'CTGAN',
     'CTGANConsistency'
 ]
-EXP_NUM = 24
+EXP_NUM = 5
 
 
-def dataset_profile(name: str, method_name: str):
+def dataset_profile(name: str, method_name: str, col_name: str):
     result_path = path.join(RESULTS_PATH, name + '/')
     reports_path = path.join(result_path + '/reports')
     pathlib.Path(reports_path).mkdir(parents=True, exist_ok=True)
 
-    real_df = pandas.read_csv(path.join(DATA_PATH, name, f'{name}_original.csv'))
-    real_profile = ProfileReport(real_df, title=f"{name}-original")
-    real_profile.to_file(path.join(reports_path, f"{name}-original"))
+    real_df = pandas.read_csv(path.join(DATA_PATH, name, f'{name}_{col_name}_original.csv'))
+    real_profile = ProfileReport(real_df, title=f"{name}-{col_name}-original")
+    real_profile.to_file(path.join(reports_path, f"{name}-{col_name}-original"))
 
     exp_num = randint(0, EXP_NUM)
-    fake_df = pandas.read_csv(path.join(DATA_PATH, name, f'{name}_{method_name}_{exp_num}.csv'))
-    fake_profile = ProfileReport(fake_df, title=f"{name}_{method_name}")
-    fake_profile.to_file(path.join(reports_path, f"{name}-{method_name}"))
+    fake_df = pandas.read_csv(path.join(DATA_PATH, name, f'{name}_{method_name}_{exp_num}_{col_name}.csv'))
+    fake_profile = ProfileReport(fake_df, title=f"{name}_{method_name}_{col_name}")
+    fake_profile.to_file(path.join(reports_path, f"{name}-{method_name}_{col_name}"))
 
 
 if __name__ == '__main__':
-    for dataset in DATASETS:
+    for dataset, cls_col, reg_col in DATASETS:
         for method in METHODS:
-            dataset_profile(dataset, method)
+            if cls_col:
+                dataset_profile(dataset, method, cls_col)
+            if reg_col:
+                dataset_profile(dataset, method, reg_col)
